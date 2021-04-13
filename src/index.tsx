8a782efa6abb32049afd1caf3edbe4c60bb6d429
Diljit VJ <diljit@locus.sh>
2021-04-13 16:55:50 +0530

    Add tinymce editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsoBYfDjaN6Ri4nnhfs8A3h6Pn7nhjcV0",
+  authDomain: "journal-af8c1.firebaseapp.com",
+  projectId: "journal-af8c1",
+  storageBucket: "journal-af8c1.appspot.com",
+  messagingSenderId: "794981906891",
+  appId: "1:794981906891:web:42a021bae920a60e5fde4e",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div
+      style={{
+        width: "1000px",
+        height: "500px",
+        border: "1px solid rgb(34,47,62)",
+        margin: "20px auto",
+      }}
+    >
+      <Editor
+        apiKey="t4kycfxgngufgebv3t8ritfj77h4r8c5yu4gcktmweqqgupb"
+        initialValue="<p>This is the initial content of the editor</p>"
+        init={{
+          skin: "snow",
+          height: 500,
+          menubar: false,
+          statusbar: false,
+          block_formats:
+            "Paragraph=p; Header 1=h1; Header 2=h2; Header 3=h3; Header 4=h4; Header 5=h5; Header 6=h6",
+          color_cols: 5,
+          plugins: [
+            "advlist autolink lists link image charmap anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media paste advlink",
+          ],
+          toolbar: `fontselect fontsizeselect | bold italic underline strikethrough superscript subscript backcolor forecolor | 
+             alignleft aligncenter alignright alignjustify lineheight | image media link`,
+          content_style: "body { font-family: Arial; }",
+        }}
+        onEditorChange={(a) => {
+          console.log(a);
+        }}
+      />
+    </div>
+  );
 };
 
 export default Home;
